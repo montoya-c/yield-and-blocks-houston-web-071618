5adea301cab300f5ff927faad0de87bb0730c51c
Cassandra Montoya <montoya.c@outlook.com>
2018-07-14 17:31:30 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,15 @@
 def hello_t (array)
   array = ["Tim", "Tom", "Jim"]
   
-  
+   i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
+end
 
 # call your method here!
 
